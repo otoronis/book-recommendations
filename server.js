15aede8923cb31eae83ad9c2056a0a49aa5c147a
otoronis <noor.sandhu1103@gmail.com>
2022-11-12 12:44:18 +0000

    set up files

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const PORT = 8000
+
+app.use()
+
+app.connect('/', (req, res) => {
+
+})
+
+app.get('/', (req, res) => {
+
+})
+
+app.put('/', (req, res) => {
+    
+})
+
+app.post('/', (req, res) => {
+    
+})
+
+app.delete('/', (req, res) => {
+    
+})
